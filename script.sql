fc244a2842ffb747f06b8034ff9ab413e53ed4ee
RobertoAndreas <roberto_andreas@hotmail.com>
2016-05-11 21:23:06 -0300

    Commit seguranca

--- script.sql
+++ script.sql
@@ -1,28 +1,38 @@
+create database dbSeguranca;
 use dbSeguranca;
 
+
 insert into tb_sistema(nm_sistema,ds_url) 
-values ('Sistema Ecommerce','http://localhost:8080/SistemaEcommerce/'),
-	   ('Seguranca','http://localhost:8080/Seguranca');
+values -- ('Sistema Ecommerce','http://localhost:8080/SistemaEcommerce/'),
+	   ('Seguranca','http://localhost:8080/Seguranca'),
+	   ('SistemaAlugaBicicleta','http://localhost:8080/SistemaAlugaBicicleta');
 
 insert into tb_usuario(ds_login,fl_ativo,ds_email,ds_senha)
 values ('carloseduardo',1,'carloseduardodantas@iftm.edu.br','e4UXW0VQYOMjfpJfAjBTypUV6Ggqg8iwmRHHJKH4t18='),
-       ('bruno',1,'bruno@iftm.edu.br','pmWkWSBCL51Bfkhn79xPuKBKHz//H6B+mY6G9/eieuM=');
+       ('roberto',1,'roberto@iftm.edu.br','pmWkWSBCL51Bfkhn79xPuKBKHz//H6B+mY6G9/eieuM=');
        
-       -- carlos
-       -- 123
-       
+              
 insert into tb_role(nm_role,cd_sistema)
-values ('ECO_admin',1),
-       ('ECO_cadastro',1),
-       ('ECO_consulta',1),
+values ('BIC_admin',1),
+       ('BIC_cadastro',1),
+       ('BIC_consulta',1),
        ('SEG_admin',2),
        ('SEG_cadastro',2),
        ('SEG_consulta',2);
        
 insert into tb_usuario_role(cd_role,ds_login)
-values (1,'bruno'),
-       (4,'bruno'),
+values (1,'roberto'),
+       (4,'roberto'),
        (3,'carloseduardo'),
        (6,'carloseduardo'); 
        
 	   
+       
+SELECT NM_ROLE, 'Roles'  
+FROM TB_ROLE R,TB_USUARIO_ROLE UR          
+WHERE R.CD_ROLE = UR.CD_ROLE
+AND UR.DS_LOGIN='roberto';
+
+select DS_SENHA
+from TB_USUARIO
+where DS_LOGIN='roberto';
---
+++ src/dao/SistemaDao.java
@@ -0,0 +1,13 @@
+package dao;
+
+import java.util.List;
+
+import seguranca.model.domain.Sistema;
+
+public interface SistemaDao {
+
+	List<Sistema> getSistemas(Sistema sistema);
+
+	
+
+}
